7b58cd288a2eb963890bab7515758b08e0032e38
Temuulen Erdenekhuu <bg.genio@gmail.com>
2019-11-14 22:34:21 -0600

    Created empty screens for future preperation

--- business-card-app/src/app/app-routing.module.ts
+++ business-card-app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path : '', redirectTo:'/dashboard', pathMatch:'full' },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- business-card-app/src/app/app.module.ts
+++ business-card-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './screens/login/login.component';
+import { NotfoundComponent } from './screens/notfound/notfound.component';
+import { HomeComponent } from './screens/home/home.component';
+import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { CreateContactComponent } from './screens/create-contact/create-contact.component';
+import { NotFoundComponent } from './screens/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotfoundComponent,
+    HomeComponent,
+    DashboardComponent,
+    CreateContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
