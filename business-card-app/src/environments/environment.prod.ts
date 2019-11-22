a47477c9b64ad56c784b03db870abf5f55f6e82f
Temuulen Erdenekhuu <bg.genio@gmail.com>
2019-11-21 22:09:58 -0600

    environment updates

--- business-card-app/src/environments/environment.prod.ts
+++ business-card-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCKQ2wpPcavV7ItU1uLQncvKAbFbCxUPaQ",
+    authDomain: "businesscardapp-50d33.firebaseapp.com",
+    databaseURL: "https://businesscardapp-50d33.firebaseio.com",
+    projectId: "businesscardapp-50d33",
+    storageBucket: "businesscardapp-50d33.appspot.com",
+    messagingSenderId: "514551253083",
+    appId: "1:514551253083:web:728b02d866b91139b2977e",
+    measurementId: "G-SP95B9LTVL"
+  }
 };
