fd5fab2e71a86fca0f49a716f7b040b50c1140d2
Temuulen Erdenekhuu <bg.genio@gmail.com>
2019-11-21 22:09:44 -0600

    integrated firebase and updated gitignore

--- business-card-app/src/app/app.module.ts
+++ business-card-app/src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
+// firebase modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './screens/login/login.component';
@@ -18,7 +25,11 @@ import { NotFoundComponent } from './screens/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule, // auth
+    AngularFirestoreModule, // firestore
+    AngularFireStorageModule, // storage
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
